ed26abf6f95272daa71b5562e6a5089ea6af7881
infiniteCable2 <steffen.dahansi.werner@googlemail.com>
2025-08-05 17:36:41 +0200

    Update packer.py safety change, set checksum 0 if not valid variant

--- opendbc/can/packer.py
+++ opendbc/can/packer.py
@@ -36,8 +36,11 @@ class CANPacker:
       self.counters[address] = (self.counters[address] + 1) % (1 << sig_counter.size)
     sig_checksum = next((s for s in msg.sigs.values() if s.type > SignalType.COUNTER), None)
     if sig_checksum and sig_checksum.calc_checksum:
-      if sig_checksum.has_checksum_variants and sig_checksum.checksum_variant_valid:
-        checksum, total = sig_checksum.calc_checksum(address, sig_checksum, dat, sig_checksum.checksum_variant)
+      if sig_checksum.has_checksum_variants:
+        if sig_checksum.checksum_variant_valid:
+          checksum, total = sig_checksum.calc_checksum(address, sig_checksum, dat, sig_checksum.checksum_variant)
+        else:
+          checksum = 0
       else:
         checksum = sig_checksum.calc_checksum(address, sig_checksum, dat)
       set_value(dat, sig_checksum, checksum)
